1a0a70dba6d11888a01c3af042153788da9909c1
Abdul Hanan Mohil <ahmohil78@gmail.com>
2023-04-25 07:32:07 -0700

    Responsive specials section

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 import {RxHamburgerMenu} from 'react-icons/rx';
 
 function Navbar(){
-    const width = window.innerWidth;
+    
     
     return(
         <div className='header'>
@@ -14,16 +14,14 @@ function Navbar(){
                 <img src={Logo}/>
             </div>
             <div className='navbar'>
-                {width>720 ?   <ul>
+                <ul>
                     <li><Link to="/little-lemon" className='nav-link'>Home</Link></li>
                     <li><Link to="/about" className='nav-link'>About</Link></li>
                     <li><Link to="/menu" className='nav-link'>Menu</Link></li>
                     <li><Link to="/booking" className='nav-link'>Reservations</Link></li>
                     <li><Link to="/order-online" className='nav-link'>Order Online</Link></li>
                     <li><Link to="/login" className='nav-link'>Login</Link></li>
-                </ul> : <div className='hamburger'> < RxHamburgerMenu size={30}/> </div>
-                }
-              
+                </ul> 
             </div>
             <div className='socialLinks'>
                 
